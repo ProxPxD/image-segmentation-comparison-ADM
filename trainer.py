5a9ae924f2d5b7a67696fdd41f2124b9e881d818
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-01-08 23:57:03 +0100

    remove retain_graph

--- trainer.py
+++ trainer.py
@@ -91,7 +91,7 @@ class Trainer:
         full_label = f'Loss - train'
         self._count(full_label, loss_result)
         self._verbosely_print(3, f'{full_label}: {loss_result}')
-        loss_result.backward(retain_graph=False)
+        loss_result.backward(retain_graph=True)
 
     def _optimize(self):
         if self.optimizer:
